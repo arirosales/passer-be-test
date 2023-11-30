01812e039f2c8ed3a736c9a0c7856da638dbfcbd
Ariany <127150460+arirosales@users.noreply.github.com>
2023-11-30 00:52:45 -0600

    Se crea tabla transaction

--- databases/postgresql.js
+++ databases/postgresql.js
@@ -6,6 +6,9 @@ const postgresql = newDb();
 postgresql.public.none(`create table users(pk_user integer, name text, status boolean);
                 insert into users values (123, 'Juan', true);`);
 
+// create table transaction
+postgresql.public.none(`create table transaction(pk_transaction integer, fk_user integer, description text, amount float);
+                insert into transaction values (1, 123, 'sinpe', 25,255);`);
 
 module.exports = {
     postgresql
